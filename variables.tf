43e77de87b90a9dab80c7ea0347c3cc9dcc99ed6
Vaibhav Gajbhe <vgajbhe22@gmail.com>
2025-05-16 18:26:31 +0530

    terraform plan

--- variables.tf
+++ variables.tf
@@ -1,20 +1,4 @@
-variable "ARM_SUBSCRIPTION_ID" {
-  type        = string
-  description = "Azure Subscription ID"
-}
-
-variable "ARM_TENANT_ID" {
-  type        = string
-  description = "Azure Tenant ID"
-}
-
-variable "ARM_CLIENT_ID" {
-  type        = string
-  description = "Azure Client ID"
-}
-
-variable "ARM_CLIENT_SECRET" {
-  type        = string
-  description = "Azure Client Secret"
-  sensitive   = true
-}
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
